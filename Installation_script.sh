14f817209a3b10598c736019e8caf3def3037aed
Raihanul Islam Bhuiyan <raihandmail@gmail.com>
2026-08-20 13:21:35 +0600

    Delete Installation_script.sh

--- Installation_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash  
-sudo apt update -y
-sudo apt install npm -y
-git clone https://github.com/nafiurrashid/react_project.git
-cd react_project
-npm install
-npm run start
-# Open the firewall to allow access to Jenkins
-sudo ufw allow 8080
