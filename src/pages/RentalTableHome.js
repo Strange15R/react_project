cd74dc1c84942d31e4ec4eea3483cf0edc39e969
Raihanul Islam Bhuiyan <raihandmail@gmail.com>
2026-08-27 11:11:26 +0600

    Update RentalTableHome.js

--- src/pages/RentalTableHome.js
+++ src/pages/RentalTableHome.js
@@ -20,7 +20,7 @@
          <ReturnProductModal />
        </div>
        <div>
-         ©2027 Sayeem || raihandmail@gmail.com || All rights reserved change5
+         ©2026 Raihan || raihandmail@gmail.com || All rights reserved
        </div>
      </div>
    );
